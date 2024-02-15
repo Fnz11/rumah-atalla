bf1982f9b9326aa2bd100d835a2fa84e52ceaada
ysFik <finz1112@gmail.com>
2024-02-15 13:24:08 +0700

    Remove vite PWA

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      strategies: "injectManifest",
-      srcDir: "src",
-      filename: "firebase-messaging-sw.js",
-      workbox: {
-        globPatterns: [],
-        globIgnores: ["*"],
-      },
-    }),
-  ],
+  plugins: [react()],
   srcDir: "./.",
 });
